063097cf3a0e284102244ac2f139bffa8d9b6f43
OlegArtyomov <o.artyomov@yaypay.com>
2018-12-17 11:04:29 +0200

    Initial loading of logic for merge big files

---
+++ src/test/java/LineAsObject.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class LineAsObject {
+
+   private Date date;
+
+   private int value;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
